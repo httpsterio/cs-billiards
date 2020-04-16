db00335faf8a9e5fa4a5ff181abed422a3ac9c99
Sami Singh <sami@httpster.io>
2020-04-17 02:07:55 +0300

    cleaned up code, added objects to a list and iterating them instead of calling the function manually each time, also added sound effect for colliding with the walls

--- Harkkatyö/Fysiikkapeli.cs
+++ Harkkatyö/Fysiikkapeli.cs
@@ -14,7 +14,7 @@ public class Harkkatyö : PhysicsGame
 
 
         // Alustetaan fysiikkaolioita. Näitä tarvitaan eri aliohjelmien välillä ja niitä välitetään parametrina toisilleen, muutoin nämä olisivat aliohjelmissaan.
-        PhysicsObject valkoinenPallo = new PhysicsObject(24,24);
+        PhysicsObject valkoinenPallo = new PhysicsObject(16,16);
         PhysicsObject maila = new PhysicsObject(17, 328);
         PhysicsObject tasku = new PhysicsObject(80,80);
 
@@ -24,18 +24,23 @@ public class Harkkatyö : PhysicsGame
         LuoMaila(maila, valkoinenPallo);
         LuoOhjaimet(maila, valkoinenPallo);
 
+        Tormaykset(valkoinenPallo);
+
+    }
+
+    public void Tormaykset(PhysicsObject tormaaja)
+    {
+        SoundEffect seinaAani = LoadSoundEffect("wall");
+
         void seinatormays(PhysicsObject pallo, PhysicsObject kohde)
         {
             if (kohde.Tag.ToString() == "laita")
             {
-                kohde.Color = RandomGen.NextColor();
+                seinaAani.Play(0.1, 0, 0);
             }
         }
-
-        AddCollisionHandler(valkoinenPallo, seinatormays);
-
+        AddCollisionHandler(tormaaja, seinatormays);
     }
-
     public void LuoOhjaimet(PhysicsObject maila, PhysicsObject valkoinenPallo)
     {
 
@@ -79,46 +84,112 @@ public class Harkkatyö : PhysicsGame
         Level.CreateBorders();
         Camera.ZoomToLevel();
 
-        // Lataa kuvasta kentän grafiikat.
+        // Lataa kuvasta kentän laidat ja luo gameobjektin.
         GameObject kentta = new GameObject(784,448);
         kentta.Image = LoadImage("poyta");
         kentta.Shape = Shape.Rectangle;
         kentta.Position = new Vector(0,0);
         Add(kentta,-1);
 
+        // Lataa kuvasta taskut ja luo siitä gameobjektin.
+        GameObject taskut = new GameObject(784, 448);
+        taskut.Image = LoadImage("taskut");
+        taskut.Shape = Shape.Rectangle;
+        taskut.Position = new Vector(0, 0);
+        Add(taskut, -2);
 
-        // Pystysuuntaiset laidat
-        LuoLaita(64, 312, new Vector(-350-11-16, 0));
-        LuoLaita(64, 312, new Vector(350+11+16, 0));
+        // Asettaa pelikentälle taustaobjektin.
+        GameObject kangas = new GameObject(784, 448);
+        kangas.Color = Color.Green;
+        kangas.Shape = Shape.Rectangle;
+        kangas.Position = new Vector(0, 0);
+        Add(kangas, -3);
 
-        // Sivusuuntaiset laidat
-        LuoLaita(296, 64, new Vector(-176, 209));
-        LuoLaita(296, 64, new Vector(-176, -209));
-        LuoLaita(296, 64, new Vector(176, 209));
-        LuoLaita(296, 64, new Vector(176, -209));
+        // Lista, jossa on laitablokkien koot (korkeus, leveys) ja sijainti vektoreina. Näitä käytetään törmäyksissä ja elementit on listassa jotta niitä voidaan kutsua loopilla.
+        var LaitaLista = new List<(double, double, Vector)>
+        {
+            (64, 312, new Vector(-376, 0)),
+            (64, 312, new Vector(376, 0)),
+            (296, 64, new Vector(-176, 209)),
+            (296, 64, new Vector(-176, -209)),
+            (296, 64, new Vector(176, 209)),
+            (296, 64, new Vector(176, -209))
+        };
 
-        // Nurkkataskut
-        LuoTasku(new Vector(-376, 200), 45);
-        LuoTasku(new Vector(376, 200), -45);
-        LuoTasku(new Vector(-376, -200), -45);
-        LuoTasku(new Vector(376, -200), 45);
+        // Iteroi listan läpi ja välittää listan itemeiden arvot parametrina funktiolle joka luo laidat
+        foreach (var item in LaitaLista)
+        {
+            LuoLaita(item.Item1, item.Item2, item.Item3);
+        }
 
-        // Keskitaskut
-        LuoTasku(new Vector(0, 228), 0);
-        LuoTasku(new Vector(0, -228), 0);
+        // Lista Nurkista joissa on törmäyksentunnistus ja pallot interaktaavat näiden kanssa.
+        var TaskuLista = new List<(Vector, double)>
+        {
+            (new Vector(-376, 200), 45),
+            (new Vector(376, 200), -45),
+            (new Vector(-376, -200), -45),
+            (new Vector(376, -200), 45),
+            (new Vector(0, 228), 0),
+            (new Vector(0, -228), 0)
+        };
+
+        // Iteroi listan läpi ja välittää listan arvot parametrina funktiolle joka luo taskut törmäyksiä varten 
+        foreach (var item in TaskuLista)
+        {
+            LuoTasku(item.Item1, item.Item2);
+        }
+
+        //// Nurkkataskut
+        //LuoTasku(new Vector(-376, 200), 45);
+        //LuoTasku(new Vector(376, 200), -45);
+        //LuoTasku(new Vector(-376, -200), -45);
+        //LuoTasku(new Vector(376, -200), 45);
+
+        // Kulmataskujen "supistajat"
+        LuoLaitaKulma(new Vector(-364, 162), 60);
+        LuoLaitaKulma(new Vector(364, 162), -60);
+        LuoLaitaKulma(new Vector(-364, -162), -60);
+        LuoLaitaKulma(new Vector(364, -162), 60);
+
+        LuoLaitaKulma(new Vector(-330, 196), 30);
+        LuoLaitaKulma(new Vector(330, 196), -30);
+        LuoLaitaKulma(new Vector(-330, -196), -30);
+        LuoLaitaKulma(new Vector(330, -196), 30);
+
+        //// Keskitaskut
+        //LuoTasku(new Vector(0, 228), 0);
+        //LuoTasku(new Vector(0, -228), 0);
 
     }
 
+    public void LuoLaitaKulma(Vector sijainti, double kallistus)
+    {
+        PhysicsObject kulma = new PhysicsObject(10, 40);
+        kulma.Shape = Shape.Rectangle;
+        kulma.Position = sijainti;
+        kulma.MakeStatic();
+        kulma.Color = Color.Red;
+        kulma.Tag = "kulma";
+        kulma.Angle = Angle.FromDegrees(kallistus);
+        Add(kulma, 3);
+
+    }
+
+    // Funktio joka luo taskun. Taskulla on törmäyksentunnistus
     public void LuoTasku(Vector sijainti, double kallistus)
     {
         PhysicsObject taskuCollision = new PhysicsObject(128, 64);
-        taskuCollision.Color = Color.Pink;
+        taskuCollision.Color = Color.Transparent;
         taskuCollision.Position = sijainti;
         taskuCollision.MakeStatic();
         taskuCollision.Shape = Shape.Rectangle;
         taskuCollision.Angle = Angle.FromDegrees(kallistus);
         taskuCollision.Tag = "taskucollision";
         Add(taskuCollision);
+        #if DEBUG
+            taskuCollision.Color = Color.Pink;
+        #endif
+
     }
 
 
@@ -128,10 +199,13 @@ public class Harkkatyö : PhysicsGame
         laita.Shape = Shape.Rectangle;
         laita.Position = sijainti;
         laita.MakeStatic();
-        laita.Color = Color.Black;
+        laita.Color = Color.Transparent;
         laita.Tag = "laita";
         Add(laita);
-        
+        #if DEBUG
+            laita.Color = Color.Blue;
+        #endif
+
     }
 
     public void LuoMaila(PhysicsObject maila, PhysicsObject valkoinenPallo)
@@ -150,7 +224,7 @@ public class Harkkatyö : PhysicsGame
         mailanAjastin.Interval = 0.016;
         mailanAjastin.Timeout += delegate()
         {
-            if (Math.Abs(valkoinenPallo.Velocity.X) > 0.5 || Math.Abs(valkoinenPallo.Velocity.Y) > 0.5)
+            if (Math.Abs(valkoinenPallo.Velocity.X) > 0.9 || Math.Abs(valkoinenPallo.Velocity.Y) > 0.9)
             {
                 maila.Size = new Vector(1,1);
             }
